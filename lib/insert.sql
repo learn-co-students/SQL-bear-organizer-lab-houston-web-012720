6fd22936fc2be6dc0a06f5f4712e3f5683ae4004
anamsoomro <anamsoomroed@gmail.com>
2020-02-05 16:39:48 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(1, 'Mr. Choclate', 3, 'female', 'brown', 'happy', TRUE),
+(2, 'Rowdy', 3, 'male', 'brown', 'mellow', FALSE),
+(3, 'Tabitha', 3, 'male', 'black', 'calm', TRUE),
+(4, 'Sergeant Brown', 3, 'male', 'white', 'mad', TRUE),
+(5, 'Melissa', 3, 'male', 'white', 'funny', FALSE),
+(6, 'Grinch', 3, 'female', 'brown', 'chill', TRUE),
+(7, 'Wendy', 3, 'male', 'black', 'wild', TRUE),
+(8, null, 3, 'female', 'black', 'wild', FALSE);
+
+
+
+
+
+
+
+
+
+
