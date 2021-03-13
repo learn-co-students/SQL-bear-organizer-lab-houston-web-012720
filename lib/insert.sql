c4c71dd9a5c8dc951cb8ed4a6477717c567b726d
SteRobWms <stephen.r.williams.87@gmail.com>
2021-03-13 13:01:51 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Bear_1", 54, "male", "brown", "grumpy", "true"),
+("Bear_2", 53, "female", "black", "sleepy", "true"),
+("Bear_3", 52, "female", "brown", "sneezy", "true"),
+("Bear_4", 52, "male", "brown", "bashful", "true"),
+("Bear_5", 10, "male", "black", "dopey", "true"),
+("Bear_6", 14, "female", "albino", "princess", "true"),
+("Bear_7", 49, "male", "black", "happy", "true"),
+(NULL, 48, "female", "brown", "doclike", "true");
